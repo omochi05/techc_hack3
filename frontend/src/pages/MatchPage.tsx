fded8828c0c6c8b37475039bce364fb421fbcf9b
omochi05 <124992638+omochi05@users.noreply.github.com>
2026-07-27 03:47:09 +0900

    feat: Task 034 試合画面にキャラクター画像と終了遷移を実装

--- frontend/src/pages/MatchPage.tsx
+++ frontend/src/pages/MatchPage.tsx
@@ -1,3 +1,7 @@
+import heroImage from "../assets/hero.png";
+import hero2Image from "../assets/hero2.png";
+import refereeImage from "../assets/referee.png";
+
 type MatchPageProps = {
   onFinish: () => void;
 };
@@ -6,11 +10,15 @@ export default function MatchPage({
   onFinish,
 }: MatchPageProps) {
   return (
-    <main>
-      <h1>試合画面</h1>
+    <main className="match-page">
+      {/* ここに後輩が作った既存の試合画面をそのまま残す */}
 
-      <button type="button" onClick={onFinish}>
-        試合終了
+      <button
+        type="button"
+        className="match-page__finish-button"
+        onClick={onFinish}
+      >
+        試合を終了する
       </button>
     </main>
   );
